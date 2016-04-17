8c28cdd306045a649e685431f833f06017449e72
Bishal Regmi <bregmi1@ramapo.edu>
2016-04-16 21:06:28 -0400

    mongoDBDone

--- src/markovharmony/DataInterface.java
+++ src/markovharmony/DataInterface.java
@@ -1,5 +1,17 @@
 package markovharmony;
 
+import org.bson.Document;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoDatabase;
+
 public class DataInterface {
 
+	public static void main(String[] args){
+		MongoClient mongoClient = new MongoClient();
+		MongoDatabase db = mongoClient.getDatabase("test");
+		db.getCollection("myCollection").insertOne(
+		        new Document("name","Bishal Regmi")
+		        		.append("Company", "Ramapo"));
+	}
 }

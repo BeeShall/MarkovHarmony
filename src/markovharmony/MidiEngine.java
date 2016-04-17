d43321bc43151206f4acbfad5fdf5e4ba183d1dd
nightCapLounge <nathaniel.c.moon@gmail.com>
2016-04-17 09:02:44 -0400

    Markovsky is a trained musician

--- src/markovharmony/MidiEngine.java
+++ src/markovharmony/MidiEngine.java
@@ -12,13 +12,88 @@ public class MidiEngine
 		contourDivisor = chordsPerMeasure;
 	}
 	
-	public void generateMidiFromProgression(ArrayList<String[]> progression)
+	public void generateMidiFromProgression(ArrayList<String[]> progression) throws InvalidMidiDataException
 	{
 		
 		
+		System.out.println("Beginning MIDI creation...");
+		midRange(progression);
+		highRange(progression);
+		
+		System.out.println("MIDI created.");
+		
+	}
+	
+	private void highRange(ArrayList<String[]> progression)
+	{
+		// Formalities
+			try
+			{
+				Sequence sequence = new Sequence(javax.sound.midi.Sequence.PPQ,24);
+				Track track = sequence.createTrack();
+				
+				byte[] b = {(byte)0xF0, 0x7E, 0x7F, 0x09, 0x01, (byte)0xF7};
+				SysexMessage sm = new SysexMessage();
+				sm.setMessage(b, 6);
+				MidiEvent me = new MidiEvent(sm,(long)0);
+				track.add(me);
+				
+				MetaMessage mt = new MetaMessage();
+				
+				mt = new MetaMessage();
+				String TrackName = new String("HighRange");
+				mt.setMessage(0x03 ,TrackName.getBytes(), TrackName.length());
+				me = new MidiEvent(mt,(long)0);
+				track.add(me);
+				
+				ShortMessage mm = new ShortMessage();
+				mm.setMessage(0xB0, 0x7D,0x00);
+				me = new MidiEvent(mm,(long)0);
+				track.add(me);
+				
+				mm = new ShortMessage();
+				mm.setMessage(0xB0, 0x7F,0x00);
+				me = new MidiEvent(mm,(long)0);
+				track.add(me);
+				
+				mm = new ShortMessage();
+				mm.setMessage(0xC0, 0x00, 0x00);
+				me = new MidiEvent(mm,(long)0);
+				track.add(me);
+
+				// Dance track
+				
+				arpeggiate(track, me, mm, progression);
+				
+				
+				// Set end of track
+				
+				mt = new MetaMessage();
+		        byte[] bet = {}; // empty array
+				mt.setMessage(0x2F,bet,0);
+				me = new MidiEvent(mt, (long)140);
+				track.add(me);
+				
+				
+				File f = new File("highRange.mid");
+				MidiSystem.write(sequence,1,f);
+				
+			
+
+			}
+			catch(Exception e)
+			{
+				System.out.println("Exception caught " + e.toString());
+			}
+		
+		}
+
+	
+	
+	private void midRange(ArrayList<String[]> progression) throws InvalidMidiDataException
+	{
 		try
 		{
-			System.out.println("Beginning MIDI creation...");
 			
 			// Formalities
 			
@@ -34,7 +109,7 @@ public class MidiEngine
 			MetaMessage mt = new MetaMessage();
 			
 			mt = new MetaMessage();
-			String TrackName = new String("Markov Demo Track");
+			String TrackName = new String("MidRange");
 			mt.setMessage(0x03 ,TrackName.getBytes(), TrackName.length());
 			me = new MidiEvent(mt,(long)0);
 			track.add(me);
@@ -68,22 +143,21 @@ public class MidiEngine
 			track.add(me);
 			
 			
-			File f = new File("midifile.mid");
+			File f = new File("midRange.mid");
 			MidiSystem.write(sequence,1,f);
 			
-			
-			System.out.println("MIDI created.");
+		
 
 		}
 		catch(Exception e)
 		{
 			System.out.println("Exception caught " + e.toString());
 		}
-		
-		
-		
-		
 	}
+	
+	
+	
+	
 	
 	private void dancePattern(Track track, MidiEvent me, ShortMessage mm, ArrayList<String[]> progression) throws InvalidMidiDataException
 	{
@@ -207,8 +281,143 @@ public class MidiEngine
 		
 	}
 	
-	
-	
-	
-	
+	private void arpeggiate(Track track, MidiEvent me, ShortMessage mm, ArrayList<String[]> progression) throws InvalidMidiDataException
+	{
+		int location = 0;
+		NoteMap notemap = new NoteMap();
+		Random rand = new Random();
+		int index1, index2;
+		int velocity = 90;
+		
+		for(int i = 0; i < progression.size(); i++)
+		{
+			for(int j = 0; j < contourDivisor; j++)
+			{			
+				
+				index1 = rand.nextInt(3);
+				index2 = rand.nextInt(3);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_ON, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				location += 6;
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_OFF, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				index1 = rand.nextInt(3);
+				index2 = rand.nextInt(3);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_ON, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				location += 6;
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_OFF, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				index1 = rand.nextInt(3);
+				index2 = rand.nextInt(3);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_ON, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				location += 6;
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_OFF, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				index1 = rand.nextInt(3);
+				index2 = rand.nextInt(3);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_ON, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				location += 6;
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_OFF, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				index1 = rand.nextInt(3);
+				index2 = rand.nextInt(3);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_ON, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				location += 6;
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_OFF, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				index1 = rand.nextInt(3);
+				index2 = rand.nextInt(3);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_ON, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				location += 6;
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_OFF, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				index1 = rand.nextInt(3);
+				index2 = rand.nextInt(3);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_ON, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				location += 6;
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_OFF, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				index1 = rand.nextInt(3);
+				index2 = rand.nextInt(3);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_ON, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				location += 6;
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_OFF, 0, notemap.Notes.get(progression.get(i)[index1])[index2], velocity);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me); 
+				
+			}			
+				
+		}
+		
+	}
+
 }

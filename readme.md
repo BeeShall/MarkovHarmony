260f90eec80703f10491bff08258a937e6881091
nightCapLounge <nathaniel.c.moon@gmail.com>
2016-04-18 03:19:04 -0400

    Update readme

--- readme.md
+++ readme.md
@@ -1,7 +1,11 @@
 
 #**Markovsky**#
 
-Markovsky uses MongoDB and Hidden Markoc Chains (with a twist) to analyze and imitate harmonic trends across artists, genres, and eras.  Markovsky writes music to imitate trends and exports the music straight to MIDI -- ready to use with your favorite DAW and capable of taking up any number of voices.  
+Markovsky uses MongoDB and Hidden Markov Chains (with a twist) to analyze and imitate harmonic trends across artists, genres, and eras.  Markovsky writes music to imitate trends and exports the music straight to MIDI -- ready to use with your favorite DAW and capable of taking up any number of voices.  
+
+-------------------------------------------------------
+
+HackRU 1st Place 
 
 -------------------------------------------------------
 
--- src/markovharmony/MidiEngine.java
+++ src/markovharmony/MidiEngine.java
@@ -19,6 +19,7 @@ public class MidiEngine
 		System.out.println("Beginning MIDI creation...");
 		midRange(progression);
 		highRange(progression);
+		openRange(progression);
 		
 		System.out.println("MIDI created.");
 		
@@ -155,6 +156,69 @@ public class MidiEngine
 		}
 	}
 	
+	private void openRange(ArrayList<String[]> progression) throws InvalidMidiDataException
+	{
+		try
+		{
+			
+			// Formalities
+			
+			Sequence sequence = new Sequence(javax.sound.midi.Sequence.PPQ,24);
+			Track track = sequence.createTrack();
+			
+			byte[] b = {(byte)0xF0, 0x7E, 0x7F, 0x09, 0x01, (byte)0xF7};
+			SysexMessage sm = new SysexMessage();
+			sm.setMessage(b, 6);
+			MidiEvent me = new MidiEvent(sm,(long)0);
+			track.add(me);
+			
+			MetaMessage mt = new MetaMessage();
+			
+			mt = new MetaMessage();
+			String TrackName = new String("Pad");
+			mt.setMessage(0x03 ,TrackName.getBytes(), TrackName.length());
+			me = new MidiEvent(mt,(long)0);
+			track.add(me);
+			
+			ShortMessage mm = new ShortMessage();
+			mm.setMessage(0xB0, 0x7D,0x00);
+			me = new MidiEvent(mm,(long)0);
+			track.add(me);
+			
+			mm = new ShortMessage();
+			mm.setMessage(0xB0, 0x7F,0x00);
+			me = new MidiEvent(mm,(long)0);
+			track.add(me);
+			
+			mm = new ShortMessage();
+			mm.setMessage(0xC0, 0x00, 0x00);
+			me = new MidiEvent(mm,(long)0);
+			track.add(me);
+
+			// Dance track
+			
+			pad(track, me, mm, progression);
+			
+			
+			// Set end of track
+			
+			mt = new MetaMessage();
+	        byte[] bet = {}; // empty array
+			mt.setMessage(0x2F,bet,0);
+			me = new MidiEvent(mt, (long)140);
+			track.add(me);
+			
+			
+			File f = new File("pad.mid");
+			MidiSystem.write(sequence,1,f);
+			
+	
+		}
+		catch(Exception e)
+		{
+			System.out.println("Exception caught " + e.toString());
+		}
+	}
 	
 	
 	
@@ -419,5 +483,60 @@ public class MidiEngine
 		}
 		
 	}
+	
+	void pad(Track track, MidiEvent me, ShortMessage mm, ArrayList<String[]> progression) throws InvalidMidiDataException
+	{
+		int location = 0;
+		NoteMap notemap = new NoteMap();
+
+		for(int i = 0; i < progression.size(); i++)
+		{
+			for(int j = 0; j < contourDivisor; j++)
+			{			
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_ON, 0, notemap.Notes.get(progression.get(i)[0])[0], 93);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_ON, 0, notemap.Notes.get(progression.get(i)[1])[0], 93);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_ON, 0, notemap.Notes.get(progression.get(i)[2])[0], 93);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				location += 48;
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_OFF, 0, notemap.Notes.get(progression.get(i)[0])[0], 93);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_OFF, 0, notemap.Notes.get(progression.get(i)[1])[0], 93);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+				
+				mm = new ShortMessage();
+				mm.setMessage(ShortMessage.NOTE_OFF, 0, notemap.Notes.get(progression.get(i)[2])[0], 93);
+				me = new MidiEvent(mm,(long)location);
+				track.add(me);
+			}			
+			
+
+			
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
 
 }
